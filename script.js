4ee1a00444c2c32081b7202633fcaa38e2ae6e95
saiveena21 <saiveenabillakanti21@gmail.com>
2025-07-09 22:18:22 +0530

    Add files via upload
    https://www.linkedin.com/posts/saiveena-billakanti-8a1a46348_apexplanet-activity-7348648067522330625-3Oq5?utm_source=share&utm_medium=member_android&rcm=ACoAAFcB6XkB21DdmI8zXChXjVag4HCoNuiMQws

---
+++ script.js
@@ -0,0 +1,5 @@
+// Define a function called showMessage
+function showMessage() {
+  // This line will display an alert message when the function is called
+  alert("Hello! You clicked the button.");
+}
